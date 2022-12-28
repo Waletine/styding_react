abbecce5702825023bd9081d5e9b7bcc8d0d9993
Belyaev <sergei.beleaev@gmail.com>
2022-12-28 15:05:19 +0200

    add project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2X77DbHd2shrBl643087z-JFSjCIwBeYBYg&usqp=CAU"></img>
+    </header>
+  );
+}
+
+export default Header;
